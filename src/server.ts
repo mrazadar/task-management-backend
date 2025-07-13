80e63a817ac2ce3488285068b18b2cbfd3cfd8ee
Raza Dar <mrazadar@gmail.com>
2025-07-13 20:39:13 +0500

    intial setup

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import env from "dotenv";
+
+env.config();
+
+const app = express();
+
+const server = () => {
+  app.use(cors());
+
+  app.use(express.json());
+
+  app.listen(process.env.PORT || 3001, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3001}`);
+  });
+};
+
+server();
