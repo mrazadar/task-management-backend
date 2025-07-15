efed4761f52e9d0e7d2b0b02e657c4229e1f2b32
Raza Dar <mrazadar@gmail.com>
2025-07-15 06:18:40 +0500

    added prisma client

--- src/prisma/client.ts
+++ src/prisma/client.ts
@@ -4,6 +4,9 @@ const prisma = new PrismaClient();
 
 /**
  * @description Prisma Client instance for database interactions.
+ * This is the main instance that is used by the application. It is
+ * initialized in the `src/index.ts` file.
+ * @see https://www.prisma.io/docs/concepts/components/prisma-client/working-with-prismaclient
  * @reference https://www.prisma.io/docs/concepts/components/prisma-client
  */
 export default prisma;
